2dd370afa1f60da198295fc291b0630e13664929
João D. Ferreira <jotomicron@gmail.com>
2019-07-08 16:42:38 +0100

    Correctly export the VS Code preferences
    The preference files are now created inside ZIP archives to ensure that
    the number of files pushed into the repository does not explode

--- preferences/populate.sh
+++ preferences/populate.sh
@@ -11,3 +11,29 @@ if wants 'gnome-terminal'; then
 
     dconf dump /org/gnome/terminal/ > preferences/gnome-terminal/config
 fi
+
+if wants 'VS Code'; then
+    mkdir -p preferences/vscode
+
+    # Create a ZIP file with the contents of ~/.vscode
+    (
+        rm -f preferences/vscode/vscode.zip
+        cd "$HOME/.vscode"
+        \ls -A | \
+            tr '\n' '\0' | \
+            xargs -0 zip -qr "$OLDPWD"/preferences/vscode/vscode.zip
+    )
+
+    # Create a ZIP file with the interesting contents of ~/.config/Code/User
+    (
+        rm -f preferences/vscode/user-config.zip
+        cd "$HOME/.config/Code/User"
+
+        # Find all user cofnig files that are not storage
+        # and add them to a ZIP file
+        \ls -A | \
+            grep -vP 'Storage$' | \
+            tr '\n' '\0' | \
+            xargs -0 zip -qr "$OLDPWD"/preferences/vscode/user-config.zip
+    )
+fi
--- setup-preferences.sh
+++ setup-preferences.sh
@@ -14,6 +14,6 @@ if wants 'VS Code'; then
     mkdir -p "$HOME/.vscode"
     mkdir -p "$HOME/.config/Code/User"
 
-    cp -rl preferences/vscode/.vscode/* "$HOME/.vscode"
-    cp -rl preferences/vscode/user-config/* "$HOME/.config/Code/User"
+    unzip -oq -d "$HOME/.vscode" preferences/vscode/vscode.zip
+    unzip -oq -d "$HOME/.config/Code/User" preferences/vscode/user-config.zip
 fi
