c300a10c8a78b755c691b183553ad649e9e95c7f
João D. Ferreira <jotomicron@gmail.com>
2019-07-08 14:14:32 +0100

    Added a mechanism to apply preferences
    This is meant to be used as a way to transfer preferences from one
    computer to the next. Each application must deal with its preferences in
    its own way, which means that there is no "right" way to handle
    preferences at the moment. Just save some file(s) on the "preferences/"
    directory and use them on the "setup-preferences.sh" script to import
    them into the current machine.

---
+++ setup-preferences.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash
+
+wants() {
+    echo -n "Do you want to install preferences for $1? [y/N] "
+    read ans
+    [ "$ans" = "y" -o "$ans" = "Y" ]
+}
+
+if wants 'gnome-terminal'; then
+    dconf load /org/gnome/terminal/ < preferences/gnome-terminal.ini
+fi
