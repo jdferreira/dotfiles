b45b0fbf9cbe7b12df5adaa49f750ea02414e85d
João D. Ferreira <jotomicron@gmail.com>
2019-07-28 18:19:50 +0100

    Correct a typo in a comment

--- copy-bin.sh
+++ copy-bin.sh
@@ -3,5 +3,5 @@
 mkdir -p "$HOME/.bin"
 
 # Copy the executables in the bin/ directory into the $HOME's .bin/ directory
-# This is automatically added tothe $PATH with the installed `.bashrc` file.
+# This is automatically added to the $PATH with the installed `.bashrc` file.
 cp -a ./bin/. "$HOME/.bin/"
